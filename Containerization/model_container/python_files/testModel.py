1abc3482e694c9b3ba6be40348de2c4103f56473
Dávid Bognár <95369319+davidbognar201@users.noreply.github.com>
2023-06-15 11:33:07 +0200

    Fixed file paths

--- Containerization/model_container/python_files/testModel.py
+++ Containerization/model_container/python_files/testModel.py
@@ -3,15 +3,15 @@ import preprocessFunctions as pf
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error, mean_absolute_percentage_error, roc_auc_score, confusion_matrix
 import pickle
 
-test_data = pd.read_csv(filepath_or_buffer = r"data/bike_rental_test_data.csv")
+test_data = pd.read_csv(filepath_or_buffer = r"/model/data/bike_rental_test_data.csv")
 test_data.rename(mapper={"cnt" : "target"}, inplace=True, axis=1)
 
-train_data = pd.read_csv(filepath_or_buffer = r"data/bike_rental_train_data.csv")
+train_data = pd.read_csv(filepath_or_buffer = r"/model/data/bike_rental_train_data.csv")
 train_data.rename(mapper={"cnt" : "target"}, inplace=True, axis=1)
 
 test_x, test_y = pf.PreprocessTestData(train_data, test_data)
 
-model = pickle.load(open(r"trained_model/GradBoosting_Regressor_trained.sav", 'rb'))
+model = pickle.load(open(r"/model/trained_model/GradBoosting_Regressor_trained.sav", 'rb'))
 predictions = model.predict(test_x)
 
 print('----------------- Results on the test set -----------------')
