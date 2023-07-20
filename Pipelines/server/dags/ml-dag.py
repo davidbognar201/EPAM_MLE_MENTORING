dfede6fdf2913044978af195b6e19efebeff6e53
Bognár Dávid <david.bognar@stud.uni-obuda.hu>
2023-07-20 14:40:19 +0200

    Created preprocessing tasks

--- Pipelines/server/dags/ml-dag.py
+++ Pipelines/server/dags/ml-dag.py
@@ -1,7 +1,5 @@
-import boto3
+
 from datetime import timedelta
-import pandas as pd
-import csv
 
 import airflow
 from airflow import DAG
@@ -9,7 +7,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 FILENAME = 'rentalData.csv'
-FILEPATH = '/data/'
+FILEPATH = 'dags/tmp_data/'
 BUCKET_NAME = 'pipelines-module-data-storage'
 VALID_COLUMNS = ["instant", "dteday", "season", "yr", "mnth", "hr", "holiday", "weekday", "workingday", "weathersit", "temp", "atemp", "hum", "windspeed", "casual", "registered", "cnt"]
 
@@ -17,15 +15,15 @@ CATEGORICAL_FEATURES = []
 NUMERIC_FEATURES = []
 
 def fetchBaseData():
-    try:
-        s3 = boto3.client('s3')
-        bucket=BUCKET_NAME
-        target_file=FILENAME
-        s3.download_file(bucket,target_file, FILEPATH + FILENAME)
-    except:
-        raise IOError("Failed to fetch data from S3 bucket - " + BUCKET_NAME)
+    import boto3 
+
+    resource = boto3.resource('s3')
+    my_bucket = resource.Bucket(BUCKET_NAME)
+    my_bucket.download_file(FILENAME, FILEPATH + FILENAME)
 
 def checkEmptyData():
+    import csv
+
     with open(FILEPATH + FILENAME) as csvfile:
             reader = csv.reader(csvfile)
             for i, _ in enumerate(reader):
@@ -34,6 +32,8 @@ def checkEmptyData():
     raise TypeError('Empty data file')
 
 def checkDataValidity():
+    import csv
+
     with open(FILEPATH + FILENAME, newline='') as f:
         reader = csv.reader(f)
         input_header = next(reader)  
@@ -48,30 +48,41 @@ def checkDataValidity():
         
 
 def checkNullValues():
+    import pandas as pd
+
     data = pd.read_csv(FILEPATH + FILENAME)
     num_of_missing = data.isnull().any(axis=1).sum()
     if num_of_missing != 0:
-        return "drop_nan_task"
+        return "drop_nan"
     else:
-        return None
+        return "categorical_preprocess"
        
 
 def dropRowsWithNullValues():
+    import pandas as pd
+
     data = pd.read_csv(FILEPATH + FILENAME)
     num_of_missing = data.isnull().any(axis=1).sum()
     data.dropna(axis=0, inplace=True)
-    print(num_of_missing + " rows were dropped due to NaN values from " + FILENAME)
+    print(str(num_of_missing) + " rows were dropped due to NaN values from " + FILENAME)
     data.to_csv(FILEPATH + FILENAME)
     print("Data file was replaced with cleaned data")
-    return None
+
 
 def categoricalPreprocessing():
+    import pandas as pd
     return None
 
 def traintestSplit():
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+
     return None
 
 def numericPreprocessing():
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+
     return None
 
 def trainModel():
@@ -92,19 +103,6 @@ default_args = {
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    # 'queue': 'bash_queue',
-    # 'pool': 'backfill',
-    # 'priority_weight': 10,
-    # 'end_date': datetime(2016, 1, 1),
-    # 'wait_for_downstream': False,
-    # 'dag': dag,
-    # 'adhoc':False,
-    # 'sla': timedelta(hours=2),
-    # 'execution_timeout': timedelta(seconds=300),
-    # 'on_failure_callback': some_function,
-    # 'on_success_callback': some_other_function,
-    # 'on_retry_callback': another_function,
-    # 'trigger_rule': u'all_success'
 }
 
 dag = DAG(
@@ -123,28 +121,49 @@ empty_data_check = PythonOperator(
     task_id='check_if_data_empty',
     python_callable=checkEmptyData,
     dag=dag)
-
 empty_data_check.set_upstream(fetch_tweets)
 
 data_validity_check = PythonOperator(
     task_id='check_if_data_is_valid',
     python_callable=checkDataValidity,
     dag=dag)
-
 data_validity_check.set_upstream(empty_data_check)
 
 data_nan_check = PythonOperator(
     task_id='check_if_data_contains_nan',
     python_callable=checkNullValues,
     dag=dag)
-
 data_nan_check.set_upstream(data_validity_check)
 
 drop_nan_task = PythonOperator(
-    task_id='drop_nan_task',
+    task_id='drop_nan',
     python_callable=dropRowsWithNullValues,
     dag=dag)
-
 drop_nan_task.set_upstream(data_nan_check)
+
+categorical_preprocess = PythonOperator(
+    task_id='categorical_preprocess',
+    python_callable=categoricalPreprocessing,
+    dag=dag)
+categorical_preprocess.set_upstream(data_nan_check)
+categorical_preprocess.set_upstream(drop_nan_task)
+
+train_test_split = PythonOperator(
+    task_id='train_test_split',
+    python_callable=traintestSplit,
+    dag=dag)
+train_test_split.set_upstream(categorical_preprocess)
+
+numerical_preprocess = PythonOperator(
+    task_id='numerical_preprocess',
+    python_callable=numericPreprocessing,
+    dag=dag)
+numerical_preprocess.set_upstream(train_test_split)
+
+train_model = PythonOperator(
+    task_id='train_model',
+    python_callable=trainModel,
+    dag=dag)
+train_model.set_upstream(numerical_preprocess)
 
 
