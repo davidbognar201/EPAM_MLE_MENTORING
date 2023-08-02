1cef1a2fb4eb2cc4935e4a1078445fcc5a3f4295
Bognár Dávid <david.bognar@stud.uni-obuda.hu>
2023-08-02 18:08:23 +0200

    Finalized the test request script

--- Model_deployment/client/src/test_request.py
+++ Model_deployment/client/src/test_request.py
@@ -1,11 +1,13 @@
 import requests
 import pandas as pd
 
-dataframe = pd.read_csv("src/data/dataset_wo_labels.csv")
-payload = dataframe.head(2).to_dict(orient="records")
+dataframe = pd.read_csv("client/src/data/dataset_wo_labels.csv")
+payload = dataframe.to_dict(orient="records")
 
 
-predictions = requests.post("http://server:8000/predict-batch", json=payload)
+predictions = requests.post("http://batch-server:8000/predict-batch", json=payload).json()
+predictions_df = pd.DataFrame.from_dict(predictions["result"])
+print(predictions_df)
+dataframe = dataframe.join(predictions_df)
+dataframe.to_csv("client/src/data/dataset_w_labels.csv")
 
-dataframe["prediction"] = predictions
-dataframe.to_csv("src/data/dataset_w_labels.csv")
