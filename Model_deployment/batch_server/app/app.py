2a1a4c5631f58d1fd08cf439a932cb19574fe8c7
Bognár Dávid <david.bognar@stud.uni-obuda.hu>
2023-08-02 18:09:01 +0200

    Updated batch app with working prediction

--- Model_deployment/batch_server/app/app.py
+++ Model_deployment/batch_server/app/app.py
@@ -1,10 +1,10 @@
-
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import FileResponse
 from pydantic import BaseModel
 import uvicorn 
 from typing import Optional, List
-from src.schema import Input
+from app.src.schema import Input
+from app.src.modules import makePrediction
 import pandas as pd
 
 app = FastAPI()
@@ -16,6 +16,10 @@ def root():
 @app.post('/predict-batch')
 async def predict_batch(inputs: List[Input]):
     input_df = pd.DataFrame([vars(s) for s in inputs])
-    return {"OK":"!!!"}
+    prediction = makePrediction(inputData=input_df,
+                                encoder_path="app/src/ml-assets/encoder.pkl",
+                                model_path="app/src/ml-assets/model.pkl",
+                                scaler_path="app/src/ml-assets/scaler.pkl")
+    return {"result":prediction.to_dict(orient="records")}
 
 
