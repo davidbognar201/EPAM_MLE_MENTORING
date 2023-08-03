85bc217331ce10792542d3bd49bc233f3dfceb02
Bognár Dávid <david.bognar@stud.uni-obuda.hu>
2023-08-03 22:33:33 +0200

    Updated batch app script to use deployment_utils package

--- Model_deployment/batch_server/app/app.py
+++ Model_deployment/batch_server/app/app.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI, File, Form, UploadFile
 from fastapi.responses import FileResponse
-from pydantic import BaseModel
 import uvicorn 
-from typing import Optional, List
-from app.src.schema import Input
-from app.src.modules import makePrediction
+from typing import List
 import pandas as pd
+# own package
+from deployment_utils.src.schemas import Input
+from deployment_utils.src.preprocessing import DataPreprocess
+
+
+
 
 app = FastAPI()
 
@@ -16,10 +19,11 @@ def root():
 @app.post('/predict-batch')
 async def predict_batch(inputs: List[Input]):
     input_df = pd.DataFrame([vars(s) for s in inputs])
-    prediction = makePrediction(inputData=input_df,
-                                encoder_path="app/src/ml-assets/encoder.pkl",
-                                model_path="app/src/ml-assets/model.pkl",
-                                scaler_path="app/src/ml-assets/scaler.pkl")
+    preprocess_ins = DataPreprocess(encoder_path="app/src/ml-assets/encoder.pkl",
+                                    scaler_path="app/src/ml-assets/scaler.pkl",
+                                    model_path="app/src/ml-assets/model.pkl")
+    
+    prediction = preprocess_ins.makePrediction(input_df)
     return {"result":prediction.to_dict(orient="records")}
 
 
