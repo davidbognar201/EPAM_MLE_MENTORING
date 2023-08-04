87212ee79307125ca39438164ee9ce7a550f2719
Bognár Dávid <david.bognar@stud.uni-obuda.hu>
2023-08-04 16:50:41 +0200

    Deleted unnecessary files

--- Model_deployment/client/src/ping_server.py
+++
@@ -1,6 +0,0 @@
-import requests
-import datetime
-#print(requests.get("http://batch-server:8000/").json())
-
-cur_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-print('JOB RUN AT : ',cur_time)
