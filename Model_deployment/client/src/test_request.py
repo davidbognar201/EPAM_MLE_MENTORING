f2dbb41358af54bd75a9657ed5a77b6e45de443d
Bognár Dávid <david.bognar@stud.uni-obuda.hu>
2023-08-04 16:51:09 +0200

    Added logging

--- Model_deployment/client/src/test_request.py
+++ Model_deployment/client/src/test_request.py
@@ -1,7 +1,9 @@
 import requests
 import pandas as pd
+import datetime
 
-dataframe = pd.read_csv("client/src/data/dataset_wo_labels.csv")
+
+dataframe = pd.read_csv("/client/src/data/dataset_wo_labels.csv")
 payload = dataframe.to_dict(orient="records")
 
 
@@ -9,8 +11,9 @@ predictions = requests.post("http://batch-server:8000/predict-batch", json=paylo
 predictions_df = pd.DataFrame.from_dict(predictions["result"])
 
 dataframe = dataframe.join(predictions_df)
-dataframe.to_csv("client/src/data/dataset_w_labels.csv")
+dataframe.to_csv("/client/src/data/dataset_w_labels.csv")
 
-print("Request successful to batch server, predicted labels are the following:")
-print(predictions_df)
+with open("/client/src/logs/request_log.txt", mode='a') as file:
+    file.write('Predicted labels are sent by batch server at %s.\n' % 
+               (datetime.datetime.now()))
 
